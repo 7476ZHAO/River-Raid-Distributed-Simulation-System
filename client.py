fd7588a088027e2f802b19c8b42ae0680c583a88
Jiali Zhao <7476zhao@gmail.com>
2025-12-01 00:46:46 -0600

    Update client.py
    replace the info of VPS

--- client.py
+++ client.py
@@ -8,9 +8,22 @@ import io
 # -----------------------------
 # SSH CONFIG
 # -----------------------------
-VPS_IP = "YOUR_VPS_IP"
-USERNAME = "YOUR_VPS_USERNAME"
-SSH_KEY = "YOUR_PRIVATE_KEY_PATH"   # You will replace this yourself
+VPS_IP = "34.139.110.221" #need to update every the vps restart
+USERNAME = "VPS101"
+SSH_KEY = "AAABAAH84RC4ydmVCzWFXuUQ6NgNk+FnWWok3+KMLn7w7XWhKo0/FxlfORS/JtpG
+oHs79ncNWdswLttpfSmS5EpO655GPLHaWzIOZ2sHwuxiJ1aW9N60KKj96WaabxjD
+wT8f/dcTxhZlDfLqbMjdfkN9003vZWbqSGs5YqxuSrlSDOpgFW7/a83Td62ZODLz
+aGtdFs2+tdJRchsDJ4x7P+rrAvPjBlylWvSBjupqpN/FxCpkgZjfngI/9vuk4WV2
+ntpJLElJwXXkoXG3kG9KbCK+Sed+ft4Ev5ZvOErdHELjLs86ldgkpLJQz3x9uu/r
+r5c18vRxkJfArTIpeAy/dClmUYEAAACBAOCD/iv49lpqbKIGZ1KcyqXp/5AUOPrm
++h6AIOFvNvkBCjDkN0Ggt1IhRjk1UWAaNjuUBkbTjtaSFkzsakG4A+kHf59TNF1R
+8b8YCk3gtUYU3RGldT+l53jlwGeNOP55Uo+gYwI2pNnBP85fJdoYAug6kzv3P1vI
+PcR7+X+1LOhHAAAAgQCkgBovPhYzKyp8A5FPo/ztTccFGwJzWGHsYMBoa7cAgjzz
+mqSfgJeP6xAzdtVXQwPl7TTswTYZK3mT8y3AUikSEGAPSGXD6F3tp+B0ai+zw9Wd
+l1h3ARyK8ZUHZsusfafoI4aQ7hCanylarf5UXTd3g7BFKkQQgDiYFZH1DtytwQAA
+AIBCHqugjw3TTVowWyY84eTM79MUrLPH4QzTolUHR1g2qmmMm4AI5JXp5Vx1ylaL
+WxpOkJ2hhowYED359U9QkT2WJ2n4VeiTNAboSNsOKwBXVk4vg7kEusuSnz9yHvEV
+CAKvP3l/ulK5WHIGmjI1zn475WOZaK12cwUeYD8WThE3kQ=="   
 GAME_STATE_FILE = "/tmp/game_state.json"
 PLAYER_ACTION_FILE = "/tmp/player_action"
 
